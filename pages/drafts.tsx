d9d322a2cf77ebb8ecfa859598330a3cddd7a081
penny.liu <penny.liu@kaiostech.com>
2021-09-14 10:03:10 +0800

    :sparkles: Create draft page

---
+++ pages/drafts.tsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import { GetServerSideProps } from 'next';
+import Layout from '../components/Layout';
+import { useSession, getSession } from 'next-auth/client';
+import {
+  Avatar,
+  Box,
+  Container,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import Router from 'next/router';
+
+type Props = {
+  drafts: PostProps[];
+};
+
+export type PostProps = {
+  id: number;
+  title: string;
+  author: {
+    name: string;
+    email: string;
+  } | null;
+  content: string;
+  published: boolean;
+  createdAt: Date;
+};
+
+const Drafts: React.FC<Props> = (props) => {
+  const [session, loading] = useSession();
+
+  if (!session) {
+    return (
+      <Layout>
+        <Flex
+          minH={'100vh'}
+          align={'center'}
+          justify={'center'}
+          bg={useColorModeValue('gray.50', 'gray.800')}
+        >
+          <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+            <Stack align={'center'}>
+              <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+              <Text fontSize={'lg'} color={'gray.600'}>
+                to see drafts <Link color={'blue.400'}></Link> ✌️
+              </Text>
+            </Stack>
+          </Stack>
+        </Flex>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <Container maxW='container.xl' py={12}>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+          <Stack spacing={4}>
+            <Text
+              textTransform={'uppercase'}
+              color={'blue.400'}
+              fontWeight={600}
+              fontSize={'sm'}
+              bg={useColorModeValue('blue.50', 'blue.900')}
+              p={2}
+              alignSelf={'flex-start'}
+              rounded={'md'}
+            >
+              {props.drafts.length !== 0 ? 'My Drafts' : 'No Drafts'}
+            </Text>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+
+      {props.drafts.map((post) => (
+        <Container
+          key={post.id}
+          maxW='container.xl'
+          onClick={() => Router.push('/p/[id]', `/p/${post.id}`)}
+        >
+          <Box
+            marginTop={{ base: '1', sm: '5' }}
+            display='flex'
+            flexDirection={{ base: 'column', sm: 'row' }}
+            justifyContent='space-between'
+          >
+            <Box
+              display='flex'
+              flex='1'
+              flexDirection='column'
+              justifyContent='center'
+              marginTop={{ base: '3', sm: '0' }}
+            >
+              <Heading marginTop='1'>
+                <Link textDecoration='none' _hover={{ textDecoration: 'none' }}>
+                  {post.title}
+                </Link>
+              </Heading>
+              <Text
+                as='p'
+                marginTop='2'
+                color={useColorModeValue('gray.700', 'gray.200')}
+                fontSize='lg'
+              >
+                {post.content}
+              </Text>
+              <BlogAuthor name={post.author.name} date={post.createdAt} />
+            </Box>
+          </Box>
+          <Divider marginTop='5' />
+        </Container>
+      ))}
+    </Layout>
+  );
+};
+
+export default Drafts;
