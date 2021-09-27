29aadd43ef5fbc6be159b9431d9b478043010539
penny.liu <penny.liu@kaiostech.com>
2021-09-27 17:38:17 +0800

    :rotating_light: Create playground files

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe('login flow', () => {
+  beforeEach(() => {
+    cy.visit('/create');
+  });
+});
---
+++ pages/playground.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import React from 'react';
+
+const Playground: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Cypress Playground</title>
+      </Head>
+    </>
+  );
+};
+
+export default Playground;
